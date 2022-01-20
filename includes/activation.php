b1dcae3f108ee1abe7b20af0c8f20780169842f2
romanb123 <romanb123@users.noreply.github.com>
2022-01-20 22:43:36 +0200

    defining creating new table on plugin activation

--- includes/activation.php
+++ includes/activation.php
@@ -8,4 +8,20 @@ function rb_activation()
     if (version_compare(get_bloginfo('version'), '5.0', '<')) {
         wp_die('you need version 5.0 or more to use this plugin');
     }
+
+
+
+    global $wpdb;
+    $createSQL      =   "
+    CREATE TABLE `" . $wpdb->prefix . "tasks` (
+        `ID` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+        `task_id` BIGINT(20) UNSIGNED NOT NULL,
+        `priority` FLOAT(3,2) UNSIGNED NOT NULL,
+        `user_ip` VARCHAR(50) NOT NULL,
+        PRIMARY KEY (`ID`)
+    ) ENGINE=InnoDB " . $wpdb->get_charset_collate() . ";";
+
+
+    require_once(ABSPATH . "/wp-admin/includes/upgrade.php");
+    dbDelta($createSQL);
 }
