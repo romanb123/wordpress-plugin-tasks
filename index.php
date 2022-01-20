71f8b273847cb51230087323d36214c065b875d3
romanb123 <romanb123@users.noreply.github.com>
2022-01-20 22:04:33 +0200

    updating the post view with tasks metadata

--- index.php
+++ index.php
@@ -16,8 +16,12 @@ if (!function_exists('add_action')) {
 include('includes/activation.php');
 include('includes/tasks_post_type.php');
 include('process/save-task-post-data.php');
+include('process/filter-tasks-post-type-content.php');
+
 // hooks
 register_activation_hook(__FILE__, 'rb_activation');
 add_action('save_post_task', 'save_tasks_post_type', 10, 3);
 add_action('init', 'Task_init');
+add_filter('the_content', 'filter_tasks_type_content');
+
 // shortcodes
---
+++ process/filter-tasks-post-type-content.php
@@ -0,0 +1,19 @@
+<?php
+function filter_tasks_type_content($content)
+{ {
+        if (is_singular('Task')) {
+            return $content;
+        }
+    }
+
+    global $post;
+    $task_data = get_post_meta($post->ID, 'Task_info', true);
+    $task_html = file_get_contents('tasks_template.php', true);
+    $task_html = str_replace('TP', __('Task Priority', 'rb-plugin'), $task_html);
+    $task_html = str_replace('Task-Priority', __('task_PRIORITY', 'rb-plugin'), $task_html);
+    // $task_html = str_replace('task_PRIORITY', $task_data['priority'], $task_html);
+    $task_html = str_replace('task_PRIORITY', $task_data['priority'], $task_html);
+
+
+    return $task_html . $content;
+}
